a4fb50322b51f181f71e080d0b5722bd12e84315
lgmarchi <lucasgmarchi@gmail.com>
2025-02-26 13:23:51 -0300

    Implement set_balance and get_balance methods in the Pallet struct

--- src/balances.rs
+++ src/balances.rs
@@ -8,4 +8,14 @@ impl Pallet {
     pub fn new() -> Self {
         Self { balances: BTreeMap::new() }
     }
+
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        self.balances.insert(who.clone(), amount);
+        unimplemented!();
+    }
+
+    pub fn get_balance(self, who: &String) -> u128 {
+        let balance = self.balances.get(who).map(|f| *f).unwrap_or(0);
+        balance
+    }
 }
