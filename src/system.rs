bd6468d5ca64f474776d8628ce4041a6ba380d5a
lgmarchi <lucasgmarchi@gmail.com>
2025-02-26 17:53:57 -0300

    feat(System): Add block number management and nonce increment functionality

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,7 @@
-use std::collections::BTreeMap;
+use std::{
+    collections::BTreeMap,
+    ops::Add,
+};
 
 pub struct Pallet {
     block_number: u32,
@@ -8,5 +11,48 @@ pub struct Pallet {
 impl Pallet {
     pub fn new() -> Self {
         Self { block_number: 0, nonce: BTreeMap::new() }
+    }
+
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    // This function can be used to increment the block number.
+    // Increases the block number by one.
+    pub fn increase_block_number(&mut self) {
+        if let Some(_) = self.block_number.checked_add(1) {
+            self.block_number = self.block_number + 1
+        }
+    }
+
+    // Increment the nonce of an account. This helps us keep track of how many
+    // transactions account has made.
+    pub fn increase_nonce(&mut self, who: &String) {
+        // let account = self.nonce.get_mut(who);
+        // if let Some(nonce_number) = account {
+        //     *nonce_number += 1;
+        // }
+
+        // let nonce = self.nonce.get(who).unwrap_or(&0);
+        // self.nonce.insert(who.clone(), nonce + 1);
+
+        // Short version
+        *self.nonce.entry(who.to_string()).or_insert(0) += 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn init_system() {
+        let system = super::Pallet::new();
+        assert_eq!(system.block_number(), 0)
+    }
+
+    #[test]
+    fn increase_block_number() {
+        let mut system = super::Pallet::new();
+        system.increase_block_number();
+        assert_eq!(system.block_number(), 1);
     }
 }
