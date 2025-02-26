fd34f98cba563043c47e2e5b1211ca41b9a49ea4
lgmarchi <lucasgmarchi@gmail.com>
2025-02-26 16:21:10 -0300

    test(Balance): Improve error handling for balance transfer with specific error messages on const variables and add more tests for overflow and insufficient balance

--- src/balances.rs
+++ src/balances.rs
@@ -1,5 +1,8 @@
 use std::collections::BTreeMap;
 
+const ERR_INSUFFICIENT_BALANCE: &str = "Insufficient balance";
+const ERR_OVERFLOW_BALANCE: &str = "Overflow when adding to balance";
+
 #[derive(Clone)]
 pub struct Pallet {
     balances: BTreeMap<String, u128>,
@@ -31,12 +34,12 @@ impl Pallet {
         let caller_balance = self.get_balance(&caller);
         let to_ballance = self.get_balance(&to);
 
-        let new_caller_balance =
-            caller_balance.checked_sub(amount).ok_or("Insufficient balance")?;
+        let new_caller_balance = caller_balance
+            .checked_sub(amount)
+            .ok_or(ERR_INSUFFICIENT_BALANCE)?;
 
-        let new_to_ballance = to_ballance
-            .checked_add(amount)
-            .ok_or("Overflow when adding to balance")?;
+        let new_to_ballance =
+            to_ballance.checked_add(amount).ok_or(ERR_OVERFLOW_BALANCE)?;
 
         self.set_balance(&caller, new_caller_balance);
         self.set_balance(&to, new_to_ballance);
@@ -47,6 +50,8 @@ impl Pallet {
 
 #[cfg(test)]
 mod tests {
+    use std::u128;
+
     const ALICE_BALANCE: &str = "Alice";
     const BOB_BALANCE: &str = "Bob";
 
@@ -75,5 +80,41 @@ mod tests {
 
         assert_eq!(balances.get_balance(&ALICE_BALANCE.to_string()), 20);
         assert_eq!(balances.get_balance(&BOB_BALANCE.to_string()), 100);
+    }
+
+    #[test]
+    fn test_balance_overflow() {
+        let mut balances = super::Pallet::new();
+
+        balances.set_balance(&ALICE_BALANCE.to_string(), 100);
+        balances.set_balance(&BOB_BALANCE.to_string(), u128::MAX);
+
+        let transfer_result = balances.transfer(
+            ALICE_BALANCE.to_string(),
+            BOB_BALANCE.to_string(),
+            1,
+        );
+
+        assert_eq!(transfer_result, Err(super::ERR_OVERFLOW_BALANCE));
+        assert_eq!(balances.get_balance(&ALICE_BALANCE.to_string()), 100);
+        assert_eq!(balances.get_balance(&BOB_BALANCE.to_string()), u128::MAX);
+    }
+
+    #[test]
+    fn insufficient_found_to_transfer() {
+        let mut balances = super::Pallet::new();
+
+        balances.set_balance(&ALICE_BALANCE.to_string(), 30);
+        balances.set_balance(&BOB_BALANCE.to_string(), 20);
+
+        let transfer_result = balances.transfer(
+            ALICE_BALANCE.to_string(),
+            BOB_BALANCE.to_string(),
+            50,
+        );
+
+        assert_eq!(transfer_result, Err(super::ERR_INSUFFICIENT_BALANCE));
+        assert_eq!(balances.get_balance(&ALICE_BALANCE.to_string()), 30);
+        assert_eq!(balances.get_balance(&BOB_BALANCE.to_string()), 20);
     }
 }
