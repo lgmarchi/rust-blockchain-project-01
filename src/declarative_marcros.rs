77f15f5011c0e481d777cdb631a72b994fafa024
lgmarchi <lucasgmarchi@gmail.com>
2025-03-07 17:58:35 -0300

    feat(Runtime): Add create_block macro for simplified block creation with extrinsics

--- src/declarative_marcros.rs
+++ src/declarative_marcros.rs
@@ -4,3 +4,23 @@ macro_rules! String {
         String::from($x)
     };
 }
+
+#[macro_export]
+macro_rules! create_block {
+    ($block_number:expr, $(($caller:expr, $to:expr, $amount:expr)),* $(,)?) => {
+        support::Block {
+            header: support::Header { block_number: $block_number },
+            extrinsics: vec![
+                $(
+                    support::Extrinsic {
+                        caller: $caller.clone(),
+                        call: RuntimeCall::Balances(balances::Call::Transfer {
+                            to: $to.clone(),
+                            amount: $amount,
+                        }),
+                    }
+                ),*
+            ],
+        }
+    };
+}
