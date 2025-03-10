ed307a8efae63be2ee76b036843bd13e363223dd
lgmarchi <lucasgmarchi@gmail.com>
2025-03-10 16:38:04 -0300

    feat(Runtime): Integrate Proof of Existence module into the runtime with dispatch logic for claims

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -1,30 +1,25 @@
 use core::fmt::Debug;
 use std::collections::BTreeMap;
 
-use crate::support::{
-    self,
-    DispatchResult,
+use crate::{
+    support::{
+        self,
+        DispatchResult,
+    },
+    utils::PoeConfig,
 };
-
-pub trait Config: crate::utils::SystemConfig {
-    /// The type which represents the content that can be claimed using this
-    /// pallet. Could be the content directly as bytes, or better yet the
-    /// hash of that content. We leave that decision to the runtime
-    /// developer.
-    type Content: Debug + Ord;
-}
 
 /// This is the Proof of Existence Module.
 /// It is a simple module that allows accounts to claim existence of some data.
 #[derive(Debug)]
-pub struct Pallet<T: Config> {
+pub struct Pallet<T: PoeConfig> {
     /// A simple storage map from content to the owner of that content.
     /// Accounts can make multiple different claims, but each claim can only
     /// have one owner.
     claims: BTreeMap<T::Content, T::AccountId>,
 }
 
-impl<T: Config> Pallet<T> {
+impl<T: PoeConfig> Pallet<T> {
     /// Create a new instance of the Proof of Existence Module.
     pub fn new() -> Self {
         Self { claims: BTreeMap::new() }
@@ -65,12 +60,12 @@ impl<T: Config> Pallet<T> {
     }
 }
 
-pub enum Call<T: Config> {
+pub enum Call<T: PoeConfig> {
     CreateClaim { claim: T::Content },
     RevokeClaim { claim: T::Content },
 }
 
-impl<T: Config> crate::support::Dispatch for Pallet<T> {
+impl<T: PoeConfig> crate::support::Dispatch for Pallet<T> {
     type Caller = T::AccountId;
     type Call = Call<T>;
 
@@ -97,7 +92,7 @@ mod tests {
 
     struct TestConfig;
 
-    impl super::Config for TestConfig {
+    impl super::PoeConfig for TestConfig {
         type Content = &'static str;
     }
 
